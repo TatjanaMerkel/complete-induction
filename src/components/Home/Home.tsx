036803f4cc1e6f9d5758357b75be4cf86b362180
dmarc001 <dmarc001@ad.local.cs.hs-rm.de>
2022-05-19 18:49:22 +0200

    [#10] feat: Added routing to develop branch, first draft of "table of contents" feature

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,22 +3,24 @@ import './Home.css';
 import Navbar from "../Navbar/Navbar";
 import HomeElement from "./HomeElement";
 import { IoSchoolSharp } from 'react-icons/io5';
+import { useNavigate } from 'react-router';
 
 function Home() {
-
+    let navigate = useNavigate();
     return (
         <div>
             <Navbar/>
                 <div className="nav-container">
 
                 <div className="menu-buttons">
-                    <HomeElement text="Tutorial" icon={require("./../../files/icons/online-learning.png")} bg_color='#BF4C4C'/>
+                    <div onClick={() => {navigate("/tutorial");}}>
+                        <HomeElement text="Tutorial" icon={require("./../../files/icons/online-learning.png")} bg_color='#BF4C4C'/>
+                    </div>
                     <HomeElement text="Aufgaben" icon={require("./../../files/icons/math_icon.png")} bg_color='#4073C3'/>
                     <HomeElement text="Spiel" icon={require("./../../files/icons/online-learning.png")} bg_color='#6FAD6E'/>
                 </div>
             </div>
         </div>
-
     );
 }
 
--- src/components/Tutorial/Tutorial.tsx
+++ src/components/Tutorial/Tutorial.tsx
@@ -1,13 +1,67 @@
 import React from "react";
 import '../Home/Home.css';
 import Navbar from "../Navbar/Navbar";
-import CCard from '../CCard/CCard';
+import TutorialCard from '../TutorialCard/TutorialCard';
+import TableOfContents from '../TableOfContents/TableOfContents';
+import { Heading } from "../TableOfContents/TableOfContents";
 
 function Tutorial() {
+    const DummyText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
     return (
         <div>
             <Navbar/>
-            <h1>This is the tutorial page!</h1>
+            {/*<TutorialCard title='Test' body="ldsfhshfslhfsoihdfsoiihf"/>*/}
+            <article>
+            <h1>The documentation page</h1>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+
+                <Heading as="h2">The introduction</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <Heading as="h2">How to get started</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <Heading as="h2">Advanced usage</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <Heading as="h3">Using upside-down</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <Heading as="h3">Easter eggs</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <Heading as="h2">Contributing</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <Heading as="h3">Your first pull request</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <Heading as="h3">Governance</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <Heading as="h2">FAQ</Heading>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+                <p>{DummyText}</p>
+            </article>
+            <TableOfContents/>
         </div>
     );
 }
