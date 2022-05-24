e3342bfc07eff13a9be6853ccd2d2b3343fe50c7
Yentl Deuster <yentlclaudia.deuster@student.hs-rm.de>
2022-05-24 19:25:53 +0200

    [#18]: teat: change structure and layout

--- src/components/BackButton/Back.tsx
+++ src/components/BackButton/Back.tsx
@@ -1,12 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import styles from './Back.module.css';
+import {useState } from "react";
 
 
+function lightenColor(color: string){
+          return "#2e3643";
+}
 
-function Back(props: { text: string;}) {
+function Back(props: { text: string; nav: number;}) {
+  const [hover, setHover] = useState(false)
   let navigate = useNavigate();
+  let ind = props.nav;
+
   return (
-      <button onClick={() => navigate(-1)}>
-          {props.text}      
+      <button className={styles.backButton} onClick={(p) => navigate(ind)} style={{backgroundColor: hover ? lightenColor("#222831"): "#222831"}} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+          {'<     '}{props.text}      
       </button>
   );
 }
