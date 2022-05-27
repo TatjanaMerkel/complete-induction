b27132b28e058868ae63a6ab0e2e0b755fbbf5b1
Tatjana Merkel <tatjana_merkel@outlook.com>
2022-05-27 19:04:38 +0200

    [#20] Add 'domino' icon for navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import './Navbar.css';
 
+
 function Navbar() {
 
     return (
         <div className="flex-container">
+            <img src={require('../../assets/dominoes.png')} alt="logo"/>
             <div className="title">
                 Vollständige Induktion
             </div>
