114206cdc75a435fa701882b30f5c5d196b07b6a
Yentl Deuster <yentlclaudia.deuster@student.hs-rm.de>
2022-05-29 13:51:44 +0200

    Merge fixes

--- src/components/TopicView/TopicView.tsx
+++ src/components/TopicView/TopicView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../Navbar/Navbar";
 import { IoSchoolSharp } from 'react-icons/io5';
 import { useNavigate } from 'react-router';
@@ -9,9 +9,16 @@ import styles from './TopicView.module.css';
 
 function TopicView() {
     let navigate = useNavigate();
+    const [childData, setState] = useState(false);
+
+
+    function handleCallback(childData: boolean) {
+        setState(childData)
+    }
+
     return (
         <div>
-            <Navbar />
+            <Navbar parentCallback ={handleCallback}/>
             <BackBar texts={["Home"]}/>
                 <div className={styles.menu_buttons}>
                     <TopicButton text="Summen" icon={require("./../../files/icons/summenzeichen.png")} />
