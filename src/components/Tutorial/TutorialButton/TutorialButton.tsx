53e5f67ba73298a48fdb1742cc43eea618af4aea
Tatjana Merkel <tatjana_merkel@outlook.com>
2022-06-01 22:48:19 +0200

    [#29] refactor: Format code

--- src/components/Tutorial/TutorialButton/TutorialButton.tsx
+++ src/components/Tutorial/TutorialButton/TutorialButton.tsx
@@ -2,38 +2,38 @@ import React from "react";
 import "./TutorialButton.css"
 
 const STYLES = [
-  "btn--primary--solid",
-  "btn--warning--solid",
-  "btn--danger--solid",
-  "btn--success--solid",
-  "btn--primary--outline",
-  "btn--warning--outline",
-  "btn--danger--outline",
-  "btn--success--outline"
+    "btn--primary--solid",
+    "btn--warning--solid",
+    "btn--danger--solid",
+    "btn--success--solid",
+    "btn--primary--outline",
+    "btn--warning--outline",
+    "btn--danger--outline",
+    "btn--success--outline"
 ];
 
 const SIZES = ["btn--medium", "btn--large"];
 
 const POSITIONS = ["btn--left", "btn--right"];
 
-export const TutorialButton = ({children, type, onClick, buttonStyle, buttonSize, buttonPosition}:any) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
+export const TutorialButton = ({children, type, onClick, buttonStyle, buttonSize, buttonPosition}: any) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle)
+        ? buttonStyle
+        : STYLES[0];
 
-  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-  const checkButtonPosition = POSITIONS.includes(buttonPosition) ? buttonPosition : POSITIONS[0];
+    const checkButtonPosition = POSITIONS.includes(buttonPosition) ? buttonPosition : POSITIONS[0];
 
-  return (
-    <button
-      className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonPosition}`}
-      onClick={onClick}
-      type={type}
-    >
-      {children}
-    </button>
-  );
+    return (
+        <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonPosition}`}
+            onClick={onClick}
+            type={type}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default TutorialButton;
--- src/components/Tutorial/TutorialCard/TutorialCard.tsx
+++ src/components/Tutorial/TutorialCard/TutorialCard.tsx
@@ -1,14 +1,14 @@
-import { ReactChild, ReactFragment, ReactPortal } from 'react';
+import {ReactChild, ReactFragment, ReactPortal} from 'react';
 import './TutorialCard.css';
 
 function TutorialCard(props: { children: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }) {
 
     return (
-            <div className="card">
-                <div className="card-body">
-                    {props.children}
-                </div>
+        <div className="card">
+            <div className="card-body">
+                {props.children}
             </div>
+        </div>
 
     );
 }
